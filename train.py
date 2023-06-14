cca8f66572f7db2c86b27370bee416f903e317f7
franziska-ott <133858571+franziska-ott@users.noreply.github.com>
2023-06-14 15:10:45 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -10,10 +10,24 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
+
 def clean_data(data):
     # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
+    months = {
+        "jan": 1,
+        "feb": 2,
+        "mar": 3,
+        "apr": 4,
+        "may": 5,
+        "jun": 6,
+        "jul": 7,
+        "aug": 8,
+        "sep": 9,
+        "oct": 10,
+        "nov": 11,
+        "dec": 12,
+    }
+    weekdays = {"mon": 1, "tue": 2, "wed": 3, "thu": 4, "fri": 5, "sat": 6, "sun": 7}
 
     # Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
@@ -37,12 +51,23 @@ def clean_data(data):
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     return x_df, y_df
 
+
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument(
+        "--C",
+        type=float,
+        default=1.0,
+        help="Inverse of regularization strength. Smaller values cause stronger regularization",
+    )
+    parser.add_argument(
+        "--max_iter",
+        type=int,
+        default=100,
+        help="Maximum number of iterations to converge",
+    )
 
     args = parser.parse_args()
 
@@ -53,10 +78,12 @@ def main():
 
     url = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
     ds = TabularDatasetFactory.from_delimited_files(url)
-    
+
     x, y = clean_data(ds)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.1, random_state=42
+    )
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
@@ -68,5 +95,6 @@ def main():
 
     run.upload_file(model_filename, model_filename)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
